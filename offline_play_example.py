9afa51c6a71d6713dbb9c4683d6bbb1aca2295f5
TonyXiao <xiaozheyuan72@gmail.com>
2025-05-09 23:00:53 +0000

    Add package change

--- offline_play_example.py
+++ offline_play_example.py
@@ -2,23 +2,58 @@ import textarena as ta
 
 # Initialize agents
 agents = {
-    0: ta.agents.OpenRouterAgent(model_name="GPT-4o-mini"),
-    1: ta.agents.OpenRouterAgent(model_name="anthropic/claude-3.5-haiku"),
+    0: ta.agents.OpenRouterAgent(model_name="llama-3.1-8b"),
+    1: ta.agents.OpenRouterAgent(model_name="llama-3.1-8b"),
 }
 
-# Initialize environment from subset and wrap it
-env = ta.make(env_id="SpellingBee-v0")
-env = ta.wrappers.LLMObservationWrapper(env=env)
-# Optional render wrapper 
-env = ta.wrappers.SimpleRenderWrapper(
-    env=env,
-    player_names={0: "GPT-4o-mini", 1: "claude-3.5-haiku"},
-)
+    
+env_list =[
+    "SpellingBee-v0",
+    "Poker-v0",
+    "SpiteAndMalice-v0",
+    "Stratego-v0",
+    "Tak-v0",
+    "TruthAndDeception-v0",
+    "UltimateTicTacToe-v0",
+    "WordChains-v0",
+    "TicTacToe-v0",
+    "Breakthrough-v0",
+    "Checkers-v0",
+    "KuhnPoker-v0",
+    "LetterAuction-v0",
+    "MemoryGame-v0",
+    "Nim-v0",
+    "Othello-v0",
+    "PigDice-v0",
+    "SimpleBlindAuction-v0",
+    "Snake-v0",
+    "SecretMafia-v0",
+    # "WildTicTacToe-v0",
+    # "ReverseTicTacToe-v0",
+    # "RandomizedTicTacToe-v0",
+    # "QuantumTicTacToe-v0",
+]
 
-env.reset(num_players=len(agents))
-done = False
-while not done:
-    player_id, observation = env.get_observation()
-    action = agents[player_id](observation)
-    done, info = env.step(action=action)
-rewards = env.close()
+
+for env_id in env_list:
+    print(f"Playing {env_id}...")
+    with open("textarena.log", "a") as log_file:
+        log_file.write(f"Playing {env_id}...\n")
+    env = ta.make(env_id=env_id)
+    env = ta.wrappers.LLMObservationWrapper(env=env)
+    # Optional render wrapper 
+    env = ta.wrappers.SimpleRenderWrapper(
+        env=env,
+        player_names={0: "GPT-4o-mini", 1: "claude-3.5-haiku"},
+    )
+
+    env.reset(num_players=len(agents))
+    done = False
+    while not done:
+        player_id, observation = env.get_observation()
+        action = agents[player_id](observation)
+        done, info = env.step(action=action)
+    rewards = env.close()
+    with open("textarena.log", "a") as log_file:
+        log_file.write(f"Rewards for {env_id}: {rewards}\n")
+    print(f'For {env_id} the rewards are {rewards}')
--- textarena/agents/basic_agents.py
+++ textarena/agents/basic_agents.py
@@ -65,11 +65,12 @@ class OpenRouterAgent(Agent):
             )
 
         # Set the open router api key from an environment variable
-        api_key = os.getenv("OPENROUTER_API_KEY")
+        # api_key = os.getenv("OPENROUTER_API_KEY")
+        api_key = "xxx"
         if not api_key:
             raise ValueError("OpenRouter API key not found. Please set the OPENROUTER_API_KEY environment variable.")
         
-        self.client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
+        self.client = OpenAI(base_url="http://localhost:8010/v1", api_key=api_key)
         # self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         
 
